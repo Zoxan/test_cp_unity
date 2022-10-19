f16c63c3aea9466e09faeab91ec1edbb38b36b74
Alexander Shutilov <avshutilov@yandex.ru>
2022-10-19 17:08:09 +0300

    test

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 public static class Build 
@@ -8,7 +11,38 @@ public static class Build
     public static void BuildAndroid()
     {
         Debug.Log("===build");
+        Bld();
+        EditorApplication.Exit(0);
         // "asdf".Bash
+    }
+
+
+    private static void Bld()
+    {
+        var target = BuildTarget.Android;
+        var outPath = "build/build.apk";
+        BuildPlayerOptions ops = new BuildPlayerOptions();
+        var scenes = EditorBuildSettings.scenes;
+        List<string> buildScenes = new List<string>();
+        foreach (EditorBuildSettingsScene settingsScene in scenes)
+        {
+            if (settingsScene.enabled)
+            {
+                buildScenes.Add(settingsScene.path);
+            }
+        }
+        ops.target = target;
+        ops.scenes = buildScenes.ToArray();
+        ops.locationPathName = outPath;
+        var report = BuildPipeline.BuildPlayer(ops);
+		
+        if (report.summary.result != UnityEditor.Build.Reporting.BuildResult.Succeeded)
+        {
+            Debug.Log("===ContinuousIntegration script build failed:");
+            // Debug.Log($"{JsonConvert.SerializeObject(report)}");
+            var msgs = report.steps.Select(x => $"{x.name}:\n{string.Join("\n", x.messages)}");
+            throw new UnityException(string.Join("\n", msgs));
+        }
     }
 
     private static void InstallGradle611()
